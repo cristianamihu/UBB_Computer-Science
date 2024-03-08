ecf7c61a6687d82b8514068af317080836d01a58
Cristiana-Maria Mihu <128689630+cristianamihu@users.noreply.github.com>
2024-03-08 20:27:04 +0200

    aplicatie agentie de turism

---
+++ Python - Fundamentele programarii/Aplicatie/Agentie de turism/Repo/Repository.py
@@ -0,0 +1,8 @@
+def adaugare_lista(lista, pachet):
+    """
+
+    :param lista: lista unde vor fi adaugate pachetele
+    :param : pachetul care va fi adaugat
+    :return: lista cu pachete
+    """
+    lista.append(pachet)
