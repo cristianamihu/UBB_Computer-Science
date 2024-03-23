75ad0a481aade24f08c2fb2c979f36ed70517b25
Cristiana-Maria Mihu <128689630+cristianamihu@users.noreply.github.com>
2024-03-23 10:54:00 +0200

    Delete Python - Fundamentele programarii/Agentie de turism directory

--- Python - Fundamentele programarii/Agentie de turism/Repo/Repository.py
+++
@@ -1,8 +0,0 @@
-def adaugare_lista(lista, pachet):
-    """
-
-    :param lista: lista unde vor fi adaugate pachetele
-    :param : pachetul care va fi adaugat
-    :return: lista cu pachete
-    """
-    lista.append(pachet)
